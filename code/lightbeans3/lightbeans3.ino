d2f95aeefb1c5527ffc61af25bd1ebe3e5ea780a
ben <limhanyangb@gmail.com>
2017-08-29 20:01:49 +0800

    fixed code

--- code/lightbeans3/lightbeans3.ino
+++ code/lightbeans3/lightbeans3.ino
@@ -2,8 +2,8 @@
 #define NUMPIXELS 12 // Set this to the number of pixels you have
 #define PIN D6 // Set this to the pin that the data of the pixel is connected to
 #define INPUT_PIN A0 // Input pin
-#define MAX_BRIGHTNESS 400.0 // Threshold value to turn on light
-#define UPDATE_TIME 10 // time between updates in ms
+#define BRIGHTNESS 400.0 // Threshold value to turn on light
+#define UPDATE_TIME 100 // time between updates in ms
 
 // Color to switch on to
 int r = 100;
@@ -11,6 +11,9 @@ int g = 30;
 int b = 70;
 
 float input_value = 0;
+
+void turn_on(int r, int g, int b);
+void turn_off();
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -25,13 +28,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   input_value = analogRead(INPUT_PIN);
-  if (input_value < BRIGHTNESS) {
+  if (input_value < BRIGHTNESS) { // if larger than our threshold value, turn on.
     turn_on(r,g,b);
   }
   if (input_value > BRIGHTNESS) {
     turn_off();
   }
-  delay(100);
+  delay(UPDATE_TIME);
 }
 
 
